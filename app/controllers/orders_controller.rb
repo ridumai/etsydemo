d41716fb585fbfe89aa2d39a35122b665d28cad2
Eneas Marin <eneas.marin@gmail.com>
2015-03-12 17:30:34 +0100

    order pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,13 +4,12 @@ class OrdersController < ApplicationController
 
   respond_to :html
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
   end
 
-  def show
-    respond_with(@order)
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
 
   def new
@@ -18,8 +17,6 @@ class OrdersController < ApplicationController
     @listing = Listing.find(params[:listing_id])
   end
 
-  def edit
-  end
 
   def create
     @order = Order.new(order_params)
@@ -40,17 +37,6 @@ class OrdersController < ApplicationController
       end
     end
   end 
-
-
-  def update
-    @order.update(order_params)
-    respond_with(@order)
-  end
-
-  def destroy
-    @order.destroy
-    respond_with(@order)
-  end
 
   private
     def set_order
